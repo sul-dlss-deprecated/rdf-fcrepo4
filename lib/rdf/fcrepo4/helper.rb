d2c2f7b09830d2efc3e4f7329b009e3093b1d2eb
Naomi Dushay <ndushay@stanford.edu>
2014-10-31 09:42:28 -0700

    remove describable type with the rest of the fedora triples

--- lib/rdf/fcrepo4/helper.rb
+++ lib/rdf/fcrepo4/helper.rb
@@ -12,6 +12,8 @@ module RDF
         # the fedora vocab is not complete and also doesn't cover modeshape triples
         fedora_ns = "http://fedora.info/definitions"
         modeshape_ns = "http://www.jcp.org/jcr"
+         # this is the old way, but just in case ...
+        fedora_describable = "http://purl.org/dc/elements/1.1/describable"
         graph.each { |stmt|  
           no_fedora_graph << stmt unless fedora_props.include?(stmt.predicate.to_s) || 
                                       fedora_props.include?(stmt.object.to_s) ||
@@ -20,7 +22,8 @@ module RDF
                                       stmt.predicate.to_s.match(modeshape_ns) || 
                                       stmt.subject.to_s.match(fedora_ns) ||
                                       stmt.object.to_s.match(fedora_ns) ||
-                                      stmt.object.to_s.match(modeshape_ns)
+                                      stmt.object.to_s.match(modeshape_ns) ||
+                                      stmt.object.to_s == (fedora_describable)
         }
         no_fedora_graph
       else
--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -7,36 +7,38 @@ describe RDF::FCRepo4 do
   
   describe '#remove_fedora_triples' do
     it 'graph returned has no fedora triples' do
-      graph = RDF::Graph.new
-      graph.from_ttl anno_ttl
+      graph = RDF::Graph.new.from_ttl anno_ttl
       expect(graph.size).to eql 39
-      result = graph.query [nil, RDF.type, RDF::URI.new("http://fedora.info/definitions/v4/rest-api#resource")]
-      expect(result.size).to eql 1
-      result = graph.query [nil, RDF.type, RDF::URI.new("http://www.jcp.org/jcr/nt/1.0base")]
-      expect(result.size).to eql 1
-      result = graph.query [nil, RDF::URI.new("http://fedora.info/definitions/v4/repository#lastModifiedBy"), nil]
-      expect(result.size).to eql 1
-      result = graph.query [nil, RDF::URI.new("http://fedora.info/definitions/v4/rest-api#writable"), nil]
-      expect(result.size).to eql 1
-      result = graph.query [RDF::URI.new("http://fedora.info/definitions/v4/repository#jcr/xml"), nil, nil]
-      expect(result.size).to eql 1
-      result = graph.query [nil, nil, RDF::URI.new("http://fedora.info/definitions/v4/repository#jcr/xml")]
-      expect(result.size).to eql 1
+      expect(graph.query([nil, RDF.type, RDF::URI.new("http://fedora.info/definitions/v4/rest-api#resource")]).size).to eql 1
+      expect(graph.query([nil, RDF.type, RDF::URI.new("http://www.jcp.org/jcr/nt/1.0base")]).size).to eql 1
+      expect(graph.query([nil, RDF::URI.new("http://fedora.info/definitions/v4/repository#lastModifiedBy"), nil]).size).to eql 1
+      expect(graph.query([nil, RDF::URI.new("http://fedora.info/definitions/v4/rest-api#writable"), nil]).size).to eql 1
+      expect(graph.query([RDF::URI.new("http://fedora.info/definitions/v4/repository#jcr/xml"), nil, nil]).size).to eql 1
+      expect(graph.query([nil, nil, RDF::URI.new("http://fedora.info/definitions/v4/repository#jcr/xml")]).size).to eql 1
 
       stripped_graph = RDF::FCRepo4.remove_fedora_triples graph
-      expect(stripped_graph.size).to eql 15
-      result = stripped_graph.query [nil, RDF.type, RDF::URI.new("http://fedora.info/definitions/v4/rest-api#resource")]
-      expect(result.size).to eql 0
-      result = stripped_graph.query [nil, RDF.type, RDF::URI.new("http://www.jcp.org/jcr/nt/1.0base")]
-      expect(result.size).to eql 0
-      result = stripped_graph.query [nil, RDF::URI.new("http://fedora.info/definitions/v4/repository#lastModifiedBy"), nil]
-      expect(result.size).to eql 0
-      result = stripped_graph.query [nil, RDF::URI.new("http://fedora.info/definitions/v4/rest-api#writable"), nil]
-      expect(result.size).to eql 0
-      result = stripped_graph.query [RDF::URI.new("http://fedora.info/definitions/v4/repository#jcr/xml"), nil, nil]
-      expect(result.size).to eql 0
-      result = stripped_graph.query [nil, nil, RDF::URI.new("http://fedora.info/definitions/v4/repository#jcr/xml")]
-      expect(result.size).to eql 0
+      expect(stripped_graph.size).to eql 14
+      expect(stripped_graph.query([nil, RDF.type, RDF::URI.new("http://fedora.info/definitions/v4/rest-api#resource")]).size).to eql 0
+      expect(stripped_graph.query([nil, RDF.type, RDF::URI.new("http://www.jcp.org/jcr/nt/1.0base")]).size).to eql 0
+      expect(stripped_graph.query([nil, RDF::URI.new("http://fedora.info/definitions/v4/repository#lastModifiedBy"), nil]).size).to eql 0
+      expect(stripped_graph.query([nil, RDF::URI.new("http://fedora.info/definitions/v4/rest-api#writable"), nil]).size).to eql 0
+      expect(stripped_graph.query([RDF::URI.new("http://fedora.info/definitions/v4/repository#jcr/xml"), nil, nil]).size).to eql 0
+      expect(stripped_graph.query([nil, nil, RDF::URI.new("http://fedora.info/definitions/v4/repository#jcr/xml")]).size).to eql 0
+    end
+    
+    it "graph returned doesn't have type http://purl.org/dc/elements/1.1/describable" do
+      g = RDF::Graph.new.from_ttl(anno_ttl)
+      expect(g.query([nil, RDF.type, RDF::URI.new("http://purl.org/dc/elements/1.1/describable")]).size).to eql 1
+      stripped_graph = RDF::FCRepo4.remove_fedora_triples g
+      expect(stripped_graph.query([nil, RDF.type, RDF::URI.new("http://purl.org/dc/elements/1.1/describable")]).size).to eql 0
+    end
+    
+    it "graph returned doesn't have type http://fedora.info/definitions/v4/rest-api#DublinCoreDescribable" do
+      ldp_cont_ttl = File.read(fixture_path + '/anno_body_ldp_container.ttl')
+      g = RDF::Graph.new.from_ttl(ldp_cont_ttl)
+      expect(g.query([nil, RDF.type, RDF::URI.new("http://fedora.info/definitions/v4/rest-api#DublinCoreDescribable")]).size).to eql 1
+      stripped_graph = RDF::FCRepo4.remove_fedora_triples g
+      expect(stripped_graph.query([nil, RDF.type, RDF::URI.new("http://fedora.info/definitions/v4/rest-api#DublinCoreDescribable")]).size).to eql 0
     end
   end
   
